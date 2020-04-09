c339de773ba2bf958ef180f15528af55e08cd245
rifathazim <work.ruhulazim@gmail.com>
2020-04-09 22:57:19 +0600

    Strings input

---
+++ ex11.py
@@ -0,0 +1,10 @@
+import math
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print(f'The value of pi is approximately {math.pi:.3f}.')
